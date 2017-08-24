a1b63a3837778c7fb2d2d992608bc80179342fd7
Doyle Young <dyoung@gmail.com>
2017-08-24 16:12:16 -0400

    Redsquare fun

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-$(document).ready(function(){
-  $('.carousel').slick({
-    "autoplay": true,
-    "arrows": true,
-    "centerMode": true
-  });
-});
-
 $("#square").click(function () {
   $("audio")[0].play();
+  $("audio")[0].addEventListener('ended', function() {
+    $("#jam h3").toggleClass("visuallyhidden");
+  }, false);
 });
